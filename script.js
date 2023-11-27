0ab5df36befd33fbf482fb128c89fad9526c4024
Lkhagvasuren Orgilsaikhan <23LP5770@G304-CC0025.local>
2023-11-27 20:32:06 +0800

    11-27

--- script.js
+++ script.js
@@ -1,39 +1,77 @@
 let containers = document.querySelectorAll(".containers")
+
+
 let container1 = document.getElementById("container1");
 let container2 = document.getElementById("container2");
 let container3 = document.getElementById("container3");
 let container4 = document.getElementById("container4");
+
+
 let levelHigh1 = document.getElementById("levelHigh1");
 let levelHigh2 = document.getElementById("levelHigh2");
 let levelHigh3 = document.getElementById("levelHigh3");
 let levelHigh4 = document.getElementById("levelHigh4");
+
+
+let firstLength = document.getElementById("firstLength");
+let secondLength = document.getElementById("secondLength");
+let thirdLength = document.getElementById("thirdLength");
+
+
+
 let levelMedium1 = document.getElementById("levelMedium1");
 let levelMedium2 = document.getElementById("levelMedium2");
 let levelMedium3 = document.getElementById("levelMedium3");
 let levelMedium4 = document.getElementById("levelMedium4");
+
+
 let levelLow1 = document.getElementById("levelLow1");
 let levelLow2 = document.getElementById("levelLow2");
 let levelLow3 = document.getElementById("levelLow3");
 let levelLow4 = document.getElementById("levelLow4");
-let h3 = document.querySelectorAll("h3");
-let bigCon = document.getElementById("big-container");
-let value = document.getElementById("status");
-let priorityValue = document.getElementById("priority");
-var taskbar = document.getElementById("taskAddBar");
-let highs = document.querySelectorAll(".highs");
-let mediums = document.querySelectorAll(".mediums")
-let lows = document.querySelectorAll(".lows")
-    let addBtn = document.querySelectorAll(".addBtn");
-        addBtn.forEach(addBtn => {
-        
-            let addingBtn = () => {
-                taskbar.style.display = "flex";
-                taskbar.style.flexDirection = "column";
-                taskbar.style.justifyContent = "space-between";
-            }
-            addBtn.addEventListener("click", addingBtn);
-        })
 
+
+let number = document.getElementsByTagName("span")
+
+
+let h3 = document.querySelectorAll("h3");
+
+
+let bigCon = document.getElementById("big-container");
+
+
+let value = document.getElementById("status");
+let coValue = document.getElementById("CoStatus")
+
+
+let priorityValue = document.getElementById("priority");
+let CoPriorityValue = document.getElementById("CoPriority")
+
+
+var taskbar = document.getElementById("taskAddBar");
+
+
+let editingBoard = document.getElementById("editing_board");
+let cancel = document.getElementById("cancel")
+
+
+let coContainers = document.querySelectorAll(".coContainer")
+
+let addBtn = document.querySelectorAll(".addBtn");
+
+
+// adding button-------------------------------------------------------------------------------------------------------------------------------------------------------
+    addBtn.forEach(addBtn => {
+        let addingBtn = () => {
+            taskbar.style.display = "flex";
+            taskbar.style.flexDirection = "column";
+            taskbar.style.justifyContent = "space-between";
+        }
+        addBtn.addEventListener("click", addingBtn);
+    })
+
+
+// adding tasks------------------------------------------------------------------------------------------------------------------------------------------------------
     let coTaskBar = document.getElementById("coAddTaskBtn");
     let coAddingBtn = () => {
         taskbar.style.display = "none";
@@ -43,53 +81,70 @@ let lows = document.querySelectorAll(".lows")
         tasks.classList.add("task")
         tasks.draggable = "true";
 
-
         let IsItDoneBtn = document.createElement("button");
-        // IsItDoneBtn.innerHTML = <i class="fa-solid fa-check" style="color: #000000;"></i>
+        IsItDoneBtn.innerHTML = `<i class="fa-solid fa-check" style="color: #000000;"></i>`
         IsItDoneBtn.classList.add("IsItDone");
         tasks.appendChild(IsItDoneBtn);
-
 
         let textTask = document.createElement("div");
         textTask.classList.add("textTask");
         tasks.appendChild(textTask);
 
-
         let deleteAndChange = document.createElement("div");
         deleteAndChange.classList.add("deleteAndChange");
         tasks.appendChild(deleteAndChange);
+        
 
 
+        let changeBtn = document.createElement("button");
         let deleteBtn = document.createElement("button");
         deleteBtn.classList.add("deleteBtn")
-        // deleteBtn.innerHTML = <i class="fa-solid fa-minus" style="color: #000000;"></i>;
-        let changeBtn = document.createElement("button");
+        deleteBtn.innerHTML = `<i class="fa-solid fa-minus" style="color: #000000;"></i>`;
         changeBtn.classList.add("changeBtn")
-        // changeBtn.innerHTML = <i class="fa-solid fa-wrench" style="color: #000000;"></i>
+        changeBtn.innerHTML = `<i class="fa-solid fa-wrench" style="color: #000000;"></i>`;
         deleteAndChange.appendChild(deleteBtn);
         deleteAndChange.appendChild(changeBtn);
 
 
-        var NameOfInput = document.getElementById("inputFirst");
+        let NameOfInput = document.getElementById("inputFirst");
         let nameOfTask = document.createElement("h4");
+
+
+
         if (NameOfInput.value === "") return false
+
+
+
         nameOfTask.innerText = NameOfInput.value;
+        nameOfTask.className = "title"
         textTask.appendChild(nameOfTask);
+
+
+        
 
 
         var Description = document.getElementById("inputSecond")
         let description = document.createElement("p");
         description.innerText = Description.value;
+        description.className = "description"
         textTask.appendChild(description);
 
+
+        let rank = document.createElement("div");
+        rank.innerText = priorityValue.value;
+        rank.classList.add("rank")
+        textTask.appendChild(rank)
+
+        
+
         if(priorityValue.value === "High"){
-            tasks.style.border = "1px solid red";
+            tasks.style.border = "3px dotted red";
         }
         if(priorityValue.value === "Medium"){
-            tasks.style.border = "1px solid blue";
+            tasks.style.border = "3px dotted blue";
         }
         if(priorityValue.value === "Low"){
-            tasks.style.border = "1px solid purple";
+            tasks.style.border = "3px dotted purple";
         }
 
 
@@ -137,14 +192,12 @@ let lows = document.querySelectorAll(".lows")
                 levelLow4.appendChild(tasks);
             }
         }
-        var dlt = document.querySelectorAll(".deleteBtn")
 
     
 
         let task = document.querySelectorAll(".task");
         task.forEach(task => {
             task.addEventListener("dragstart", () => {
-                console.log("dragstart")
                 task.classList.add("dragging");
             });
             task.addEventListener("dragend", () => {
@@ -152,39 +205,114 @@ let lows = document.querySelectorAll(".lows")
             })
 
             let GoToDone = () => {
-                container4.appendChild(task)
+                if(priorityValue.value === "High"){
+                    levelHigh4.appendChild(tasks)
+                }
+                if(priorityValue.value === "Medium"){
+                    levelMedium4.appendChild(tasks)
+                }
+                if(priorityValue.value === "Low"){
+                    levelLow4.appendChild(tasks)
+                }
             }
 
 
             IsItDoneBtn.addEventListener("click", GoToDone)
         })
-        console.log(highs)
-        highs.forEach(highs => {
-            highs.addEventListener("dragover", e => {
+            
+        
+        
+            container1.addEventListener("dragover", e => {
                 e.preventDefault();
-                console.log("dragover")
                 let draggable = document.querySelector(".dragging");
                 if(priorityValue.value === "High"){
-                    highs.appendChild(draggable)
+                    levelHigh1.appendChild(draggable)
+                }
+                if(priorityValue.value === "Medium"){
+                    levelMedium1.appendChild(draggable)
+                }
+                if(priorityValue.value === "Low"){
+                    levelLow1.appendChild(draggable)
+                }
+            })
+            container2.addEventListener("dragover", e => {
+                e.preventDefault();
+                let draggable = document.querySelector(".dragging");
+                if(priorityValue.value === "High"){
+                    levelHigh2.appendChild(draggable)
+                }
+                if(priorityValue.value === "Medium"){
+                    levelMedium2.appendChild(draggable)
+                }
+                if(priorityValue.value === "Low"){
+                    levelLow2.appendChild(draggable)
+                }
+            })
+
+            container3.addEventListener("dragover", e => {
+                e.preventDefault();
+                let draggable = document.querySelector(".dragging");
+                if(priorityValue.value === "High"){
+                    levelHigh3.appendChild(draggable)
+                }
+                if(priorityValue.value === "Medium"){
+                    levelMedium3.appendChild(draggable)
+                }
+                if(priorityValue.value === "Low"){
+                    levelLow3.appendChild(draggable)
+                }
+            })
+
+            container4.addEventListener("dragover", e => {
+                e.preventDefault();
+                let draggable = document.querySelector(".dragging");
+                if(priorityValue.value === "High"){
+                    levelHigh4.appendChild(draggable)
+                }
+                if(priorityValue.value === "Medium"){
+                    levelMedium4.appendChild(draggable)
+                }
+                if(priorityValue.value === "Low"){
+                    levelLow4.appendChild(draggable)
                 }
             })
 
 
-
-
-            let changingRef = () => {
-                taskbar.style.display = "flex";
-
-                task.forEach(task => {
-                    containers.removeChild(task)
-                })
+            const titleInput = document.querySelector(".title")
+            const descriptionInput = document.querySelector(".description")
+        coContainers.forEach(coContainers => {
+            let deletingRef = () => {
+                coContainers.removeChild(tasks)
+            }
+            deleteBtn.addEventListener("click", deletingRef)
+            })
+            let nameValue = NameOfInput.value;
+            let descriptionValue = Description.value;
+            let CoInputFirst = document.getElementById("CoInputFirst");
+            let CoInputSecond = document.getElementById("CoInputSecond");
+            let editingBtn = document.getElementById("editingBtn");
+            changeBtn.onclick = () => {
+                coValue.value = value.value;
+                CoPriorityValue.value = priorityValue.value;
+                editingBoard.style.display = "flex"
+                editingBoard.style.flexDirection = "column";
+                CoInputFirst.value = nameValue;
+                CoInputSecond.value = descriptionValue;
+                console.log(coValue.value)
+                editingBtn.onclick = () => {
+                    titleInput.textContent = CoInputFirst.value;
+                    descriptionInput.textContent = CoInputSecond.value;
+                    value.value = coValue.value;
+                    priorityValue.value = CoPriorityValue.value
+                    editingBoard.style.display = "none";
+                }
             }
 
-            
-            changeBtn.addEventListener("click", changingRef)
-        })
-        NameOfInput.value = "";
-        Description.value = ""
 
+
+        NameOfInput.value = "";
+        Description.value = "";
     }
     coTaskBar.addEventListener("click", coAddingBtn);
+    // let num = levelLow1.childElementCount;
+    // console.log(num)
