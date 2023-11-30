47e81bbbac11d56bfba7a83526270dbd6457eb31
Lkhagvasuren Orgilsaikhan <23LP5770@G304-CC0025.local>
2023-11-30 19:25:02 +0800

    11-30 shineeer goyoor todo bicij bn

--- script1.js
+++ script1.js
@@ -1,17 +1,99 @@
 let pushTaskRef = document.getElementById("push-task-btn");
 let addTaskRefs = document.querySelectorAll(".add-ref");
 let taskAddBar = document.getElementById("task-add-bar");
-let cancelTaskAddBar = document.getElementById("cancel-01");
 let firstTextArea = document.getElementById("first-textArea");
 let secondTextArea = document.getElementById("second-textArea");
 let coFirstTextArea = document.getElementById("co-first-textArea");
 let coSecondTextArea = document.getElementById("co-second-textArea");
-let statusFirst = document.getElementById("status")
+let statusFirst = document.getElementById("status");
+let priorityFirst = document.getElementById("priority")
+let todoList = document.getElementById("todo-list");
+let inprogressList = document.getElementById("inprogress-list");
+let stuckList = document.getElementById("stuck-list");
+let doneList = document.getElementById("done-list");
+let rankNum;
+let rankNumArr = [];
+
+let state = {
+    TodoTasks: [],
+    InProgressTaks: [],
+    StuckTasks: [],
+    DoneTasks: []
+}
+
+let sorting = () => {
+    rankNum = Number(priorityFirst.value);
+    rankNumArr.push(rankNum)
+    rankNumArr.sort(function(a, b){return b.priority - a.priority})
+}
+
+let refreshingArray = () => {
+    if (statusFirst.value === "ToDo") {
+        state.TodoTasks.push(
+            {title: firstTextArea.value},
+            {description: secondTextArea.value},
+            {address: statusFirst.value},
+            {rank: priorityFirst.value}
+            )
+    }
+    if (statusFirst.value === "InProgress") {
+        state.InProgressTaks.push(
+            {title: firstTextArea.value},
+            {description: secondTextArea.value},
+            {address: statusFirst.value},
+            {rank: priorityFirst.value}
+            )
+    }
+    if (statusFirst.value === "Stuck") {
+        state.StuckTasks.push(
+            {title: firstTextArea.value},
+            {description: secondTextArea.value},
+            {address: statusFirst.value},
+            {rank: priorityFirst.value}
+            )
+    }
+    if (statusFirst.value === "Done") {
+        state.DoneTasks.push(
+            {title: firstTextArea.value},
+            {description: secondTextArea.value},
+            {address: statusFirst.value},
+            {rank: priorityFirst.value}
+            )
+    }
+}
+
+let render = () => {
+    let title = document.createElement("h3");
+    let description = document.createElement("p");
+    let deleteBtn = document.createElement("button");
+    let task = document.createElement("div");
+    task.classList.add("task");
+    let taskClass = document.querySelectorAll(".task");
+    title.innerText = firstTextArea.value;
+    description.innerText = secondTextArea.value;
+    deleteBtn.innerHTML = `<i class="fa-solid fa-minus" style="color: #000000;"></i>`
+    task.appendChild(title);
+    task.appendChild(description);
+    task.appendChild(deleteBtn);
+    if (statusFirst.value === "ToDo") todoList.appendChild(task);
+    if (statusFirst.value === "InProgress") inprogressList.appendChild(task);
+    if (statusFirst.value === "Stuck") stuckList.appendChild(task);
+    if (statusFirst.value === "Done") doneList.appendChild(task);
+    sorting();
+    // deleteBtn.onclick = () => {
+    //         if (statusFirst.value === "ToDo"){
+    //             todoList.removeChild(task);
+    //         }
+    //         if (statusFirst.value === "InProgress") inprogressList.removeChild(task);
+    //         if (statusFirst.value === "Stuck") stuckList.removeChild(task);
+    //         if (statusFirst.value === "Done") doneList.removeChild(task);
+    // }
+}
 
 addTaskRefs.forEach(addTaskRefs => {
-
-
     let displayTaskBar = () =>{
+        firstTextArea.value = "";
+        secondTextArea.value = "";
         taskAddBar.style.display = "flex";
         if (addTaskRefs.id === "first-ref") statusFirst.value = "ToDo";
         if (addTaskRefs.id === "second-ref") statusFirst.value = "InProgress";
@@ -22,19 +104,13 @@ addTaskRefs.forEach(addTaskRefs => {
 })
 
 
-let creatingTask = () => {
-    let task = document.createElement("div");
-    task.classList
+
+let pushingTasks = () => {
+    render();
+    refreshingArray();
+    console.log(rankNumArr);
+    taskAddBar.style.display = "none";
 }
 
 
-
-
-cancelTaskAddBar.onclick = () => {
-    taskAddBar.style.display = "none"
-}
-
-let pushingTaskToContainers = () => {
-    
-}
-pushTaskRef.addEventListener("click", pushingTaskToContainers)
+pushTaskRef.addEventListener("click", pushingTasks)
